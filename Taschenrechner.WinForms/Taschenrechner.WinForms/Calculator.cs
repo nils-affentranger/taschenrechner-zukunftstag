e13a78a4c230171233732baa9f4aa5101707ef15
nils-affentranger <nils.affentranger@exanic.ch>
2024-08-07 09:36:50 +0200

    Implement button Input

---
+++ Taschenrechner.WinForms/Taschenrechner.WinForms/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner.WinForms {
+
+    internal class Calculator {
+        private string currentCalculation;
+
+        public bool AddCharacter(string character) {
+            if (IsValidCharacter(character)) {
+                if (IsOperator(character) && IsOperator(currentCalculation.Last().ToString())) {
+                    currentCalculation = currentCalculation.Remove(currentCalculation.Length - 1);
+                }
+                currentCalculation = currentCalculation + character;
+                return true;
+            }
+            else return false;
+        }
+
+        public void Clear() {
+            currentCalculation = string.Empty;
+        }
+
+        public string GetCurrentCalculation() {
+            return currentCalculation;
+        }
+
+        private bool IsOperator(string character) {
+            return character == "+" || character == "*" || character == "-" || character == "/";
+        }
+
+        private bool IsValidCharacter(string character) {
+            if (string.IsNullOrEmpty(character)) {
+                return false;
+            }
+            if (int.TryParse(character, out _)) {
+                return true;
+            }
+            if (IsOperator(character)) {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
--- Taschenrechner.WinForms/Taschenrechner.WinForms/CalculatorForm.cs
+++ Taschenrechner.WinForms/Taschenrechner.WinForms/CalculatorForm.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 
 namespace Taschenrechner.WinForms {
     public partial class CalculatorForm : Form {
+
+        Calculator c = new Calculator();
+
         public CalculatorForm() {
             InitializeComponent();
         }
@@ -23,55 +26,55 @@ namespace Taschenrechner.WinForms {
         }
 
         private void button0_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("0");
         }
 
         private void button1_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("1");
         }
 
         private void button2_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("2");
         }
 
         private void button3_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("3");
         }
 
         private void button4_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("4");
         }
 
         private void button5_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("5");
         }
 
         private void button6_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("6");
         }
 
         private void button7_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("7");
         }
 
         private void button8_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("8");
         }
 
         private void button9_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("9");
         }
         private void buttonDivide_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("/");
         }
         private void buttonMultiply_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("*");
         }
         private void buttonSubtract_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("-");
         }
         private void buttonAdd_Click(object sender, EventArgs e) {
-
+            c.AddCharacter("+");
         }
         private void buttonSolve_Click(object sender, EventArgs e) {
 
