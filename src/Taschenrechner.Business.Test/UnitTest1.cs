a9cb302cc84ec0e361fde940e72ef0d1f14c4dd8
Nils Affentranger <nils@nanumpf.com>
2024-08-14 10:09:40 +0200

    Add Taschenrechner.Business.Test

---
+++ src/Taschenrechner.Business.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Bogus;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Taschenrechner.WinForms;
+
+namespace Taschenrechner.Business.Test {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void NumbersTest() {
+            var fake = new Faker();
+            var sut = new Calculator();
+
+            Assert.IsTrue(sut.AddCharacter(fake.Random.Number(0, 9).ToString()));
+        }
+    }
+}
