b9eaa677f9a89ec65ed91d0181a09bda3e7abb9b
Nils Affentranger <nils@nanumpf.com>
2024-08-14 13:57:53 +0200

    add historyTest and clearTest

--- src/Taschenrechner.Business.Test/CalculatorTest.cs
+++ src/Taschenrechner.Business.Test/CalculatorTest.cs
@@ -7,27 +7,16 @@ namespace Taschenrechner.Business.Test {
     public class CalculatorTest {
 
         [TestMethod]
-        public void NumbersTest() {
+        public void clearTest() {
             var fake = new Faker();
             var sut = new Calculator();
 
-            Assert.IsTrue(sut.AddCharacter(fake.Random.Number(0, 9).ToString()));
-        }
+            sut.AddCharacter(fake.Random.Number(1, 9).ToString());
+            sut.AddCharacter(fake.Random.Number(1, 9).ToString());
+            sut.AddCharacter(fake.PickRandom<string>("+", "-", "*", "/", "^"));
+            sut.Clear();
 
-        [TestMethod]
-        public void firstOperatorTest() {
-            var fake = new Faker();
-            var sut = new Calculator();
-
-            Assert.IsFalse(sut.AddCharacter(fake.PickRandom<string>("+", "-", "*", "/", "^")));
-        }
-
-        [TestMethod]
-        public void invalidCharTest() {
-            var fake = new Faker();
-            var sut = new Calculator();
-
-            Assert.IsFalse(sut.AddCharacter(fake.Random.String(length:1)));
+            Assert.AreEqual(string.Empty, sut.GetCurrentCalculation());
         }
 
         [TestMethod]
@@ -49,6 +38,48 @@ namespace Taschenrechner.Business.Test {
         }
 
         [TestMethod]
+        public void firstOperatorTest() {
+            var fake = new Faker();
+            var sut = new Calculator();
+
+            Assert.IsFalse(sut.AddCharacter(fake.PickRandom<string>("+", "-", "*", "/", "^")));
+        }
+
+        [TestMethod]
+        public void historyTest() {
+            var fake = new Faker();
+            var sut = new Calculator();
+
+            sut.AddCharacter(fake.Random.Number(1, 9).ToString());
+            sut.AddCharacter(fake.PickRandom<string>("+", "-", "*", "/", "^"));
+            sut.AddCharacter("(");
+            sut.AddCharacter(fake.Random.Number(1, 9).ToString());
+            sut.AddCharacter(fake.PickRandom<string>("+", "-", "*", "/", "^"));
+            sut.AddCharacter(fake.Random.Number(1, 9).ToString());
+            sut.AddCharacter(")");
+
+            var result = sut.Evaluate();
+            var history = sut.HistoryString;
+
+            Assert.AreEqual(result, history);
+        }
+
+        [TestMethod]
+        public void invalidCharTest() {
+            var fake = new Faker();
+            var sut = new Calculator();
+
+            Assert.IsFalse(sut.AddCharacter(fake.Random.String(length: 1)));
+        }
+
+        [TestMethod]
+        public void NumbersTest() {
+            var fake = new Faker();
+            var sut = new Calculator();
+
+            Assert.IsTrue(sut.AddCharacter(fake.Random.Number(0, 9).ToString()));
+        }
+        [TestMethod]
         public void orderOfOperationsTest() {
             var sut = new Calculator();
 
@@ -61,20 +92,6 @@ namespace Taschenrechner.Business.Test {
             var result = sut.Evaluate();
 
             Assert.AreEqual("23", result);
-        }
-
-
-        [TestMethod]
-        public void clearTest() {
-            var fake = new Faker();
-            var sut = new Calculator();
-
-            sut.AddCharacter(fake.Random.Number(1, 9).ToString());
-            sut.AddCharacter(fake.Random.Number(1, 9).ToString());
-            sut.AddCharacter(fake.PickRandom<string>("+", "-", "*", "/", "^"));
-            sut.Clear();
-
-            Assert.AreEqual(string.Empty, sut.GetCurrentCalculation());
         }
     }
 }
