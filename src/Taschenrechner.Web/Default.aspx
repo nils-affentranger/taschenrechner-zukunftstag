fe63352fb0d0c31cc61262f04f2f708cfe5c7234
Nils Affentranger <nils@nanumpf.com>
2024-08-21 09:09:21 +0200

    add Taschenrechner.Web

---
+++ src/Taschenrechner.Web/Default.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="Home Page" Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Taschenrechner.Web._Default" %>
+
+<div>Hallo Welt</div>
---
+++ src/Taschenrechner.Web/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Taschenrechner.Web {
+
+    public class Global : HttpApplication {
+    }
+}
