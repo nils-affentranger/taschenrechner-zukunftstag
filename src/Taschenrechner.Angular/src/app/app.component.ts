31ee01920db104c4653e78a81a9b0e03bddc2bac
Nils Affentranger <nils@nanumpf.com>
2024-09-12 17:51:04 +0200

    temp

--- src/Taschenrechner.Angular/src/app/app.component.ts
+++ src/Taschenrechner.Angular/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CalculationDisplayComponent } from './calculation-display/calculation-display.component';
 import { ButtonGridComponent } from './buttons/button-grid/button-grid.component';
+import {HistoryBoxComponent} from "./history-box/history-box.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CalculationDisplayComponent, ButtonGridComponent],
+  imports: [CommonModule, RouterOutlet, CalculationDisplayComponent, ButtonGridComponent, HistoryBoxComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
