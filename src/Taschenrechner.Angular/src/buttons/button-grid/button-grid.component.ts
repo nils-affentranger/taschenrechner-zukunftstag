57b1f3f3e2a0f223077226651c571859efe85a81
Nils Affentranger <nils@nanumpf.com>
2024-09-12 09:21:57 +0200

    add essential components

---
+++ src/Taschenrechner.Angular/src/buttons/button-grid/button-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './button-grid.component.html',
+  styleUrl: './button-grid.component.scss'
+})
+export class ButtonGridComponent {
+
+}
---
+++ src/Taschenrechner.Angular/src/calculation-display/calculation-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculation-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculation-display.component.html',
+  styleUrl: './calculation-display.component.scss'
+})
+export class CalculationDisplayComponent {
+
+}
