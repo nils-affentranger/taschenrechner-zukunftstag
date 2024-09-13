45adf2c455cbc82b477e72be3ad954444eff2c57
Nils Affentranger <nils@nanumpf.com>
2024-09-13 09:19:06 +0200

    add history-box.component

--- src/Taschenrechner.Angular/src/app/app.component.ts
+++ src/Taschenrechner.Angular/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CalculationDisplayComponent } from './calculation-display/calculation-display.component';
+import { HistoryBoxComponent } from "./history-box/history-box.component";
 import { ButtonGridComponent } from './buttons/button-grid/button-grid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CalculationDisplayComponent, ButtonGridComponent],
+  imports: [CommonModule, RouterOutlet, CalculationDisplayComponent, ButtonGridComponent, HistoryBoxComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/Taschenrechner.Angular/src/app/history-box/history-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-history-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './history-box.component.html',
+  styleUrl: './history-box.component.scss'
+})
+export class HistoryBoxComponent {
+
+}
