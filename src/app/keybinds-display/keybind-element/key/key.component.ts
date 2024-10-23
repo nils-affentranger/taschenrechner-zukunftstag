a651cbcaa2dba2e74b365e501042ddb958091047
Nils Affentranger <nils@nanumpf.com>
2024-10-23 16:40:31 +0200

    clear repository

--- src/app/keybinds-display/keybind-element/key/key.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-key',
-  standalone: true,
-  imports: [],
-  templateUrl: './key.component.html',
-  styleUrl: './key.component.scss',
-})
-export class KeyComponent {}
