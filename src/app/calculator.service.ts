4d91b8f8356a12b0489a35d4f5f5d3e962c3764e
Nils Affentranger <nils.affentranger@exanic.ch>
2025-10-22 14:00:51 +0200

    before angular update

--- src/app/calculator.service.ts
+++ src/app/calculator.service.ts
@@ -1,69 +1,71 @@
-import { effect, Injectable, signal, WritableSignal } from '@angular/core';
-import { create, all } from 'mathjs';
-import { last } from 'rxjs';
+import { effect, Injectable, signal, WritableSignal } from "@angular/core";
+import { create, all } from "mathjs";
+import { last } from "rxjs";
 
 const math = create(all);
 var originalDivide = math.divide;
-math.import({
-  divide: function (a: number, b: number) {
-    if (math.isZero(b)) {
-      throw new Error('Cannot devide by zero');
-    }
-    return originalDivide(a, b);
-  }
-}, {override: true})
+math.import(
+  {
+    divide: function (a: number, b: number) {
+      if (math.isZero(b)) {
+        throw new Error("Cannot devide by zero");
+      }
+      return originalDivide(a, b);
+    },
+  },
+  { override: true }
+);
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class CalculatorService {
-
   private lastActionWasEvaluation = false;
 
   // Typen
-  private numbers = '0123456789';
-  private operators = '+-*/^';
-  private parentheses = '()';
+  private numbers = "0123456789";
+  private operators = "+-*/^";
+  private parentheses = "()";
 
   // Den aktuellen Zustand aus dem Browser abrufen
   currentCalculation: WritableSignal<string> = signal(
-    localStorage.getItem('currentCalculation') || '',
+    localStorage.getItem("currentCalculation") || ""
   );
   history: WritableSignal<string[]> = signal(
-    JSON.parse(localStorage.getItem('history') || '[]'),
+    JSON.parse(localStorage.getItem("history") || "[]")
   );
 
   // #region constructor
   constructor() {
     // Den aktuellen Zustand im Browser speichern
     effect(() => {
-      localStorage.setItem('currentCalculation', this.currentCalculation());
+      localStorage.setItem("currentCalculation", this.currentCalculation());
     });
 
     effect(() => {
-      localStorage.setItem('history', JSON.stringify(this.history()));
+      localStorage.setItem("history", JSON.stringify(this.history()));
     });
 
     // math.js (math.evaluate() Funktion) für präzisere Ergebnisse konfigurieren
     math.config({
-      number: 'BigNumber',
+      number: "BigNumber",
       precision: 13,
     });
   }
   // #endregion
 
   determineType(
-    character: string,
-  ): 'number' | 'operator' | 'parenthesis' | 'undefined' {
+    character: string
+  ): "number" | "operator" | "parenthesis" | "undefined" {
     if (this.numbers.includes(character)) {
-      return 'number';
+      return "number";
     } else if (this.operators.includes(character)) {
-      return 'operator';
+      return "operator";
     } else if (this.parentheses.includes(character)) {
-      return 'parenthesis';
+      return "parenthesis";
     } else {
       console.error(`Unknown character: ${character}`);
-      return 'undefined';
+      return "undefined";
     }
   }
 
@@ -72,14 +74,14 @@ export class CalculatorService {
     const lastChar = calc.slice(-1);
     const type = this.determineType(char);
 
-    if (calc == '' && type == 'operator') {
+    if ((calc == "" || this.lastActionWasEvaluation) && type == "operator") {
       return;
     }
 
     // Wenn die letzte Aktion eine Auswertung war, Rechnung löschen
     if (this.lastActionWasEvaluation) {
       this.clear();
-      calc = '';
+      calc = "";
     }
 
     // Verhindern, dass am Anfang der Rechnung ein Operator hinzugefügt wird
@@ -96,7 +98,7 @@ export class CalculatorService {
 
   evaluate() {
     // Nur auswerten, wenn es etwas auszuwerten gibt
-    if (this.currentCalculation() !== '') {
+    if (this.currentCalculation() !== "") {
       try {
         let result = math.evaluate(this.currentCalculation());
         this.currentCalculation.set(result.toString());
@@ -104,7 +106,7 @@ export class CalculatorService {
         // Geteilt durch 0 besser handhaben
 
         // Das Resultat nur dem Verlauf hinzufügen, wenn es keine Fehler gab
-        if (!['NaN', 'Infinity'].includes(this.currentCalculation())) {
+        if (!["NaN", "Infinity"].includes(this.currentCalculation())) {
           this.history.update((history) => {
             const newHistory = [this.currentCalculation(), ...history];
             // Verhindern, dass die Länge des Verlaufes nicht 10 Zeilen überschreitet
@@ -116,17 +118,16 @@ export class CalculatorService {
           this.lastActionWasEvaluation = true;
         }
       } catch (e) {
-        let message = 'Cannot divide by zero';
-        if (e instanceof Error && e.message === 'Cannot devide by zero')
-        {
+        let message = "Cannot divide by zero";
+        if (e instanceof Error && e.message === "Cannot devide by zero") {
           this.currentCalculation.set(message);
-        } 
+        }
         // Calculation-display rot erblitzen lassen
         const displayBackground =
-          document.getElementById('display-background')!;
-        displayBackground.classList.add('error');
+          document.getElementById("display-background")!;
+        displayBackground.classList.add("error");
         setTimeout(() => {
-          displayBackground.classList.remove('error');
+          displayBackground.classList.remove("error");
         }, 50);
         console.error(e);
       }
@@ -140,13 +141,13 @@ export class CalculatorService {
       .pop();
 
     // Nur einen Dezimalpunkt hinzufügen, wenn die letzte Zahl noch keinen enthält.
-    if (lastNumber && !lastNumber.includes('.')) {
-      this.currentCalculation.set(this.currentCalculation() + '.');
+    if (lastNumber && !lastNumber.includes(".")) {
+      this.currentCalculation.set(this.currentCalculation() + ".");
     }
   }
 
   clear() {
-    this.currentCalculation.set('');
+    this.currentCalculation.set("");
   }
 
   clearEntry() {
@@ -161,7 +162,7 @@ export class CalculatorService {
     // backspace() wiederholen, bis das letzte zeichen nicht mehr eine Zahl ist
     while (this.numbers.includes(this.currentCalculation().slice(-1))) {
       this.backspace();
-      if (this.currentCalculation().slice(-1) === '') {
+      if (this.currentCalculation().slice(-1) === "") {
         break;
       }
     }
@@ -169,7 +170,7 @@ export class CalculatorService {
 
   backspace() {
     // Wenn die letzte Auswertung zu Fehlern führte, Berechnung löschen
-    if (['NaN', 'Infinity'].includes(this.currentCalculation())) {
+    if (["NaN", "Infinity"].includes(this.currentCalculation())) {
       this.clear();
       return;
     }
@@ -189,14 +190,14 @@ export class CalculatorService {
       const lastNumber = match[0];
       const index = match.index!;
 
-      if (lastNumber.startsWith('-')) {
+      if (lastNumber.startsWith("-")) {
         this.currentCalculation.set(
           // Die Rechnung ohne Minuszeichen speichern
-          calculation.slice(0, index) + lastNumber.slice(1),
+          calculation.slice(0, index) + lastNumber.slice(1)
         );
       } else {
         this.currentCalculation.set(
-          calculation.slice(0, index) + '-' + lastNumber,
+          calculation.slice(0, index) + "-" + lastNumber
         );
       }
     }
