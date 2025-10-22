47d203b3a5e8b09845e1c7f9b4cca7d02f850dbd
Nils Affentranger <nils.affentranger@exanic.ch>
2025-10-22 16:43:50 +0200

    remove zone.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  provideZonelessChangeDetection,
+} from "@angular/core";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [provideZonelessChangeDetection()],
 };
